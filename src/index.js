a74f36d522e3a17b0b2c39d5010b9e0e164fdcc5
henih <86572062+henih@users.noreply.github.com>
2024-01-18 15:37:39 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateMantra(event) {
+  event.preventDefault();
 
+  new Typewriter("#mantra", {
+    strings: "Your Mantra will appear here",
+    autoStart: true,
+    delay: 90,
+    cursor: " 🕉️",
+  });
+}
+
+let mantraFormElement = document.querySelector("#form-input");
+mantraFormElement.addEventListener("submit", generateMantra);
