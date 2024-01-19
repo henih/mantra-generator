6106d04542aac0ef764c93eeb43c13b186f26ccf
henih <86572062+henih@users.noreply.github.com>
2024-01-19 16:44:51 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -13,13 +13,15 @@ function generateMantra(event) {
   let instructionsInput = document.querySelector("#user-instructions");
 
   let key = "oa503cb248d231a90f14935068e8t36f";
-  let prompt = `Write a positive affirmation with the topic of ${instructionsInput.value}"`;
+  let prompt = `Write a positive affirmation with the topic of ${instructionsInput.value}`;
   let context =
     "You are a yoga AI assistant who is focused on wellbeing and mindfullness. Please make your answer short and sweet. Please behave.";
   let URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
-  console.log("AI is running");
-  console.log(`Prompt: ${prompt}`);
+  let mantraElement = document.querySelector("#mantra");
+  mantraElement.style.display = "block";
+  mantraElement.innerHTML = `<div class="blink">🧘🏼‍♀️ Generating your mantra about ${instructionsInput.value}</div>`;
+
 
   axios.get(URL).then(displayMantra);
 }
