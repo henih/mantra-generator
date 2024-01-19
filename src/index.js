1b20b5b91a5076d3ed0fef184b20b0fec32906cd
henih <86572062+henih@users.noreply.github.com>
2024-01-19 16:12:00 +0100

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateMantra(event) {
-  event.preventDefault();
-
+function displayMantra(response) {
   new Typewriter("#mantra", {
-    strings: "Your Mantra will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 90,
     cursor: " 🕉️",
   });
+}
+
+function generateMantra(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let key = "oa503cb248d231a90f14935068e8t36f";
+  let prompt = `Write a positive affirmation with the topic of ${instructionsInput.value}"`;
+  let context =
+    "You are a yoga AI assistant who is focused on wellbeing and mindfullness. Please make your answer short and sweet. Please behave.";
+  let URL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  console.log("AI is running");
+  console.log(`Prompt: ${prompt}`);
+
+  axios.get(URL).then(displayMantra);
 }
 
 let mantraFormElement = document.querySelector("#form-input");
